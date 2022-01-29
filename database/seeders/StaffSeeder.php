06a03d9ad9a396b2a7c767de8e5ddfbf518cea07
Novandi Kevin Pratama <kevinpret27@gmail.com>
2022-01-29 18:32:23 +0700

    Update

--- database/seeders/StaffSeeder.php
+++ database/seeders/StaffSeeder.php
@@ -7,11 +7,7 @@ use Illuminate\Database\Seeder;
 
 class StaffSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
+
     public function run()
     {
         User::create([
